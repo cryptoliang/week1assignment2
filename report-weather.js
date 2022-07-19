9b4f0ef445587e61a0e3a008fe726a98ff5cc44e
Liang Chen <liang.chen@crypto.com>
2022-07-19 23:24:59 +0800

    report weathers one by one

--- report-weather.js
+++ report-weather.js
@@ -3,37 +3,59 @@ const abi = require("./abi")
 
 require("dotenv").config()
 
-const cities = ["shanghai", "hongkong", "london"]
-
 const WEATHER_CONTRACT_ADDRESS = "0x49354813d8BFCa86f778DfF4120ad80E4D96D74E"
 
-async function main() {
-    const weathers = await Promise.all(
+const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL)
+const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
+const weatherContract = new ethers.Contract(WEATHER_CONTRACT_ADDRESS, abi, wallet)
+
+async function queryWeathers(cities) {
+    console.log(`Querying weathers from https://goweather.herokuapp.com/weather`)
+    return await Promise.all(
         cities.map(async (city) => {
             const response = await ethers.utils.fetchJson(`https://goweather.herokuapp.com/weather/${city}`)
+            console.log(`Got ${city}: ${response.temperature}`)
             return {
                 city: city,
                 temperature: parseInt(response.temperature),
             }
         })
     )
+}
 
-    const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL)
-    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
-
-    const weatherContract = new ethers.Contract(WEATHER_CONTRACT_ADDRESS, abi, wallet)
-
-    const batchId = 54212
+async function reportWeathers(batchId, weathers) {
+    console.log(`Reporting weathers to contract address: ${WEATHER_CONTRACT_ADDRESS}`)
 
     for (const weather of weathers) {
-        let txResponse = await weatherContract.reportWeather(batchId, weather.city, weather.temperature)
-        await txResponse.wait(1)
+        let txResponse = await weatherContract.reportWeather(
+            batchId,
+            ethers.utils.formatBytes32String(weather.city),
+            weather.temperature
+        )
+        let txReceipt = await txResponse.wait(1)
+        console.log(
+            `Reported ${weather.city}'s temperature: ${weather.temperature} at tx: ${txReceipt.transactionHash}`
+        )
     }
+}
 
+async function readWeathersFromContract(batchId, cities) {
+    console.log(`Reading weathers from contract address: ${WEATHER_CONTRACT_ADDRESS}`)
     for (const city of cities) {
-        let txResponse = await weatherContract.getWeather(batchId, city)
-        console.log(txResponse)
+        let temperature = await weatherContract.getWeather(batchId, ethers.utils.formatBytes32String(city))
+        console.log(`Got ${city}: ${temperature}`)
     }
+}
+
+async function main() {
+    const cities = ["shanghai", "hongkong", "london"]
+    const batchId = Math.round(Date.now() / 1000)
+
+    const weathers = await queryWeathers(cities)
+    console.log("------------------------------")
+    await reportWeathers(batchId, weathers)
+    console.log("------------------------------")
+    await readWeathersFromContract(batchId, cities)
 }
 
 main().catch((e) => console.log(e))
